652efd16ae71ba637ff87bb0d86a917dc512747a
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-09-04 20:00:34 +0300

    Fix mistake in sin square function. NOTE: SinSquare compute its value belong to x-axis

--- src/solvers/BoundaryConditions.cpp
+++ src/solvers/BoundaryConditions.cpp
@@ -13,7 +13,7 @@ SinSquareFunction::SinSquareFunction(const double A)
 
 double SinSquareFunction::value(const Point<3> &p, const size_t /*component*/) const
 {
-    const double sin_square = 1 - (p[2] * p[2]) / p.norm_square();
+    const double sin_square = p[0] * p[0] / p.norm_square();
 
     return A * sin_square;
 }
