c0bab10c3f21787a94e47f4901c57b84ddeb3b2f
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-12-15 23:12:55 +0300

    Remove f-strings in order to launch on python 3.5

--- run_directrory.py
+++ run_directrory.py
@@ -12,14 +12,16 @@ class TaskRunner:
 
     def run(self):
         for task in sorted(list(self.directory.iterdir())):
-            print(f'Start task: {task.name}')
+            print('Start task: {0}'.format(task.name))
             start_time = time()
             self.run_task(task)
-            print(f'Task: {task.name} takes \n', time() - start_time)
+            print('Task: {0} takes \n'.format(task.name), time() - start_time)
 
     def run_task(self, task: Path):
         output_dir = self.create_and_return_output_dir(task)
-        command = [f'{self.executable}', f'--out={output_dir}', f'--task={task}']
+        command = [str(self.executable),
+                   '--out={0}'.format(output_dir),
+                   '--task={0}'.format(task)]
         subprocess.run(command)
 
     def create_and_return_output_dir(self, task: Path):
