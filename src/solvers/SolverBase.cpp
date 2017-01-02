65978b4ebc2307fcb51e56561e0d947265742475
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-02 21:41:57 +0300

    Add empty lines in logger to improve readability.

--- src/solvers/SolverBase.cpp
+++ src/solvers/SolverBase.cpp
@@ -24,7 +24,8 @@ void SolverBase::do_logged_do_postprocessing(const std::string &output_dir)
     dealii::deallog << "Results will be written in " << output_dir << std::endl;
     this->do_postprocessing(output_dir);
     auto end = system_clock::now();
-    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl;
+    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl
+                    << std::endl;
 }
 
 void SolverBase::do_logged_solve_linear_system()
@@ -34,7 +35,8 @@ void SolverBase::do_logged_solve_linear_system()
     const size_t n_iter = this->solve_linear_system();
     dealii::deallog << "Solver converges in " << n_iter << " iterations." << std::endl;
     auto end = system_clock::now();
-    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl;
+    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl
+                    << std::endl;
 }
 
 void SolverBase::do_logged_assemble_system()
@@ -43,7 +45,8 @@ void SolverBase::do_logged_assemble_system()
     dealii::deallog << "Assembling system..." << std::endl;
     this->assemble_system();
     auto end = system_clock::now();
-    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl;
+    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl
+                    << std::endl;
 }
 
 void SolverBase::do_logged_setup_system()
@@ -53,5 +56,6 @@ void SolverBase::do_logged_setup_system()
     this->setup_system();
     dealii::deallog << "Number of degrees of freedom: " << this->get_n_dofs() << std::endl;
     auto end = system_clock::now();
-    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl;
+    dealii::deallog << "It takes: " << duration_cast<duration<double>>(end - start).count() << "s" << std::endl
+                    << std::endl;
 }
