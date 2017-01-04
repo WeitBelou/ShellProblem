6b7ef523c36ddc78f39b4c5fe405d780b76f9c19
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-04 23:34:47 +0300

    Wrap task execution with try-catch.

--- src/launcher/Launcher.cpp
+++ src/launcher/Launcher.cpp
@@ -13,6 +13,14 @@ void Launcher::run(const std::string &task_file, const std::string &output_dir)
     deallog.depth_console(1);
     deallog.attach(file_logstream);
 
-    auto task = TaskFactory::create_task_from_json(TaskUtil::read_json_from_file(task_file), output_dir);
-    task->execute();
+    try {
+        auto task = TaskFactory::create_task_from_json(TaskUtil::read_json_from_file(task_file), output_dir);
+        task->execute();
+    }
+    catch (std::exception &e) {
+        deallog << e.what() << std::endl;
+    }
+    catch (...) {
+        deallog << "Something happened..." << std::endl;
+    }
 }
