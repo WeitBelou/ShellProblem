c7c0a74f667a6549e2633a97cc79e5be44b45bab
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-05 21:02:28 +0300

    Remove unused functions.

--- src/mesh/MeshUtilities.cpp
+++ src/mesh/MeshUtilities.cpp
@@ -11,44 +11,28 @@
 
 bool MeshUtilities::is_point_on_sphere(const dealii::Point<3> &point,
                                        const dealii::Point<3> &center,
-                                       const double radius,
-                                       const double tolerance)
+                                       const double radius)
 {
-    return fuzzy_equal((point - center).norm(), radius, tolerance);
+    return fuzzy_equal((point - center).norm(), radius);
 }
 
 bool MeshUtilities::is_point_on_plane(const dealii::Point<3> &point,
                                       const dealii::Point<3> &point_on_plane,
-                                      const unsigned int axis,
-                                      const double tolerance)
+                                      const unsigned int axis)
 {
     //Plane: (n, r) == (n, r_0)
     const dealii::Point<3> axis_vector = get_axis_from_number(axis);
-    return fuzzy_equal(point * axis_vector, point_on_plane * axis_vector, tolerance);
-
-}
-
-bool MeshUtilities::is_point_on_circle(const dealii::Point<3> &point,
-                                       const dealii::Point<3> &center,
-                                       const double radius,
-                                       const unsigned int axis,
-                                       const double tolerance)
-{
-    if (!is_point_on_plane(point, center, axis, tolerance)) {
-        return false;
-    }
-    return is_point_on_sphere(point, center, radius, tolerance);
+    return fuzzy_equal(point * axis_vector, point_on_plane * axis_vector);
 
 }
 
 bool MeshUtilities::is_face_on_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
                                       const dealii::Point<3> &center,
-                                      const double radius,
-                                      const double tolerance)
+                                      const double radius)
 {
     for (unsigned int v = 0; v < dealii::GeometryInfo<3>::vertices_per_face; ++v) {
         const dealii::Point<3> current_point = face->vertex(v);
-        if (!is_point_on_sphere(current_point, center, radius, tolerance)) {
+        if (!is_point_on_sphere(current_point, center, radius)) {
             return false;
         }
     }
@@ -57,50 +41,11 @@ bool MeshUtilities::is_face_on_sphere(const dealii::Triangulation<3>::active_fac
 
 bool MeshUtilities::is_face_on_plane(const dealii::Triangulation<3>::active_face_iterator &face,
                                      const dealii::Point<3> &point_on_plane,
-                                     const unsigned int axis,
-                                     const double tolerance)
+                                     const unsigned int axis)
 {
     for (unsigned int v = 0; v < dealii::GeometryInfo<3>::vertices_per_face; ++v) {
         const dealii::Point<3> current_point = face->vertex(v);
-        if (!is_point_on_plane(current_point, point_on_plane, axis, tolerance)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool MeshUtilities::is_face_inscribed_in_circle(const dealii::Triangulation<3>::active_face_iterator &face,
-                                                const dealii::Point<3> &center,
-                                                const double radius,
-                                                const unsigned int axis,
-                                                const double tolerance)
-{
-    for (unsigned int v = 0; v < dealii::GeometryInfo<3>::vertices_per_face; ++v) {
-        const dealii::Point<3> current_point = face->vertex(v);
-        if (!is_point_on_circle(current_point, center, radius, axis, tolerance)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool MeshUtilities::is_face_on_cylinder(const dealii::Triangulation<3>::active_face_iterator &face,
-                                        const dealii::Point<3> &center,
-                                        const unsigned int axis,
-                                        const double radius,
-                                        const double length,
-                                        const double tolerance)
-{
-    dealii::Point<3> axis_vector = get_axis_from_number(axis);
-
-    for (unsigned int v = 0; v < dealii::GeometryInfo<3>::vertices_per_face; ++v) {
-        const dealii::Point<3> shifted_vertex = face->vertex(v) + (-center);
-        const double distance_to_axis = (shifted_vertex - (axis_vector * shifted_vertex) * axis_vector).norm();
-
-        if (!fuzzy_equal(distance_to_axis, radius, tolerance)) {
-            return false;
-        }
-        else if (shifted_vertex(axis) > length / 2 || shifted_vertex(axis) < -length / 2) {
+        if (!is_point_on_plane(current_point, point_on_plane, axis)) {
             return false;
         }
     }
@@ -121,7 +66,7 @@ dealii::Point<3> MeshUtilities::get_axis_from_number(const unsigned int axis_no)
     return dealii::Point<3>();
 }
 
-bool MeshUtilities::fuzzy_equal(const double x, const double y, const double tolerance)
+bool MeshUtilities::fuzzy_equal(const double x, const double y)
 {
-    return std::fabs(x - y) < tolerance;
+    return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
 }
--- src/mesh/MeshUtilities.hpp
+++ src/mesh/MeshUtilities.hpp
@@ -7,44 +7,22 @@ class MeshUtilities
 public:
     static bool is_point_on_sphere(const dealii::Point<3> &point,
                                    const dealii::Point<3> &center,
-                                   const double radius,
-                                   const double tolerance = 1e-10);
+                                   const double radius);
 
     static bool is_point_on_plane(const dealii::Point<3> &point,
                                   const dealii::Point<3> &point_on_plane,
-                                  const unsigned int axis,
-                                  const double tolerance = 1e-10);
-
-    static bool is_point_on_circle(const dealii::Point<3> &point,
-                                   const dealii::Point<3> &center,
-                                   const double radius,
-                                   const unsigned int axis,
-                                   const double tolerance = 1e-10);
+                                  const unsigned int axis);
 
     static bool is_face_on_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
                                   const dealii::Point<3> &center,
-                                  const double radius,
-                                  const double tolerance = 1e-10);
+                                  const double radius);
 
     static bool is_face_on_plane(const dealii::Triangulation<3>::active_face_iterator &face,
                                  const dealii::Point<3> &point_on_plane,
-                                 const unsigned int axis,
-                                 const double tolerance = 1e-10);
+                                 const unsigned int axis);
 
-    static bool is_face_inscribed_in_circle(const dealii::Triangulation<3>::active_face_iterator &face,
-                                            const dealii::Point<3> &center,
-                                            const double radius,
-                                            const unsigned int axis,
-                                            const double tolerance = 1e-10);
-
-    static bool is_face_on_cylinder(const dealii::Triangulation<3>::active_face_iterator &face,
-                                    const dealii::Point<3> &center,
-                                    const unsigned int axis,
-                                    const double radius,
-                                    const double length,
-                                    const double tolerance = 1e-10);
 private:
-    static bool fuzzy_equal(const double x, const double y, const double tolerance);
+    static bool fuzzy_equal(const double x, const double y);
     static dealii::Point<3> get_axis_from_number(const unsigned int axis_no);
 };
 
