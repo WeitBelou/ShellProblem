1e00ae0cc306b82f9417158f45bd7016a7b65ba3
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-28 15:48:48 +0300

    Issue with material markers. Add minimal test case.

--- src/materials/MaterialsGroup.cpp
+++ src/materials/MaterialsGroup.cpp
@@ -6,5 +6,7 @@ void MaterialsGroup::add_material(dealii::types::boundary_id id, std::shared_ptr
 
 const std::shared_ptr<const Material> MaterialsGroup::get_material_by_id(dealii::types::material_id id) const
 {
+    AssertThrow(materials.count(id) != 0,
+                dealii::ExcInternalError(std::string("Element with id: ") + std::to_string(id) + " not in materials"));
     return materials.find(id)->second;
 }
