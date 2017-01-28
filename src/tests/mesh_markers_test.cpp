dbff69cf8f171a29b8278e204f3f6dda3f6a9079
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-28 19:11:41 +0300

    Add test.

--- src/tests/mesh_markers_test.cpp
+++ src/tests/mesh_markers_test.cpp
@@ -1,1 +1,15 @@
+#include "shell/mesh/CubeMesh.hpp"
+#include "shell/mesh_markers/TwoPlanesMarker.hpp"
 #include "gtest/gtest.h"
+TEST(MaterialMarkersCubeTest, SingleMaterial)
+{
+    MeshMarkersGroup markers;
+    markers.add_marker(std::make_shared<const TwoPlanesMarker>(1, dealii::Point<3>(0, 0, -0.5),
+                                                               dealii::Point<3>(0, 0, 0.5), 2));
+    CubeMesh mesh(1.0, dealii::Point<3>(0, 0, 0), 0, markers);
+
+    auto tria = mesh.mesh();
+    for (auto cell: tria.active_cell_iterators()) {
+        ASSERT_EQ(cell->material_id(), 1);
+    }
+}
