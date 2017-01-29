16d9b198c678dcc4446c341753e2b4cf8ba33191
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-29 20:30:48 +0300

    Add manifold descriptors for legs.

--- src/shell/mesh/IceIslandWithLegsMesh.cpp
+++ src/shell/mesh/IceIslandWithLegsMesh.cpp
@@ -1,4 +1,6 @@
 #include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <src/shell/util/MeshUtilities.hpp>
 #include "IceIslandWithLegsMesh.hpp"
 IceIslandWithLegsMesh::IceIslandWithLegsMesh(unsigned int n_global_refinements,
                                              const MeshMarkersGroup &markers,
@@ -46,5 +48,48 @@ void IceIslandWithLegsMesh::create_coarse_mesh()
 
 void IceIslandWithLegsMesh::apply_manifold_ids()
 {
+    const unsigned n = 11;
+    const double c = a / 11.0;
 
+    static const dealii::FlatManifold<3> flat_manifold;
+    const dealii::Point<3> &z_axis = dealii::Point<3>(0, 0, 1);
+    static const dealii::CylindricalManifold<3> left_cylinder(z_axis,
+                                                              dealii::Point<3>(3.5 * c, 5.5 * c, a));
+
+    static const dealii::CylindricalManifold<3> right_cylinder(z_axis,
+                                                               dealii::Point<3>(7.5 * c, 5.5 * c, a));
+
+    static const dealii::CylindricalManifold<3> top_cylinder(z_axis,
+                                                             dealii::Point<3>(5.5 * c, 7.5 * c, a));
+    static const dealii::CylindricalManifold<3> bottom_cylinder(z_axis,
+                                                                dealii::Point<3>(5.5 * c, 3.5 * c, a));
+
+    tria.set_manifold(0, flat_manifold);
+    tria.set_manifold(1, left_cylinder);
+    tria.set_manifold(2, right_cylinder);
+    tria.set_manifold(3, top_cylinder);
+    tria.set_manifold(4, bottom_cylinder);
+
+    tria.set_all_manifold_ids(0);
+    for (auto cell : tria.active_cell_iterators()) {
+        const dealii::Point<3> center = cell->center();
+        if (center(2) > a) {
+            if (MeshUtilities::fuzzy_equal(center(1), 5.5 * c)) {
+                if (center(0) < a / 2) {
+                    cell->set_all_manifold_ids(1);
+                }
+                else {
+                    cell->set_all_manifold_ids(2);
+                }
+            }
+            else {
+                if (center(1) > a / 2) {
+                    cell->set_all_manifold_ids(3);
+                }
+                else {
+                    cell->set_all_manifold_ids(4);
+                }
+            }
+        }
+    }
 }
--- src/shell/util/MeshUtilities.hpp
+++ src/shell/util/MeshUtilities.hpp
@@ -1,51 +1,49 @@
 #pragma once
 #include "deal.II/grid/tria.h"
 
-class MeshUtilities
+namespace MeshUtilities
 {
-public:
-    static bool is_point_on_plane(const dealii::Point<3> &point,
-                                  const dealii::Point<3> &point_on_plane,
-                                  const unsigned int axis);
+bool is_point_on_plane(const dealii::Point<3> &point,
+                       const dealii::Point<3> &point_on_plane,
+                       const unsigned int axis);
 
-    static bool is_face_on_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
-                                  const dealii::Point<3> &center,
-                                  const double radius);
+bool is_face_on_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
+                       const dealii::Point<3> &center,
+                       const double radius);
 
-    static bool is_face_on_half_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
-                                       const dealii::Point<3> &center,
-                                       const double radius,
-                                       const dealii::Point<3> &orientation);
+bool is_face_on_half_sphere(const dealii::Triangulation<3>::active_face_iterator &face,
+                            const dealii::Point<3> &center,
+                            const double radius,
+                            const dealii::Point<3> &orientation);
 
-    static bool is_face_on_plane(const dealii::Triangulation<3>::active_face_iterator &face,
-                                 const dealii::Point<3> &point_on_plane,
-                                 const unsigned int axis);
+bool is_face_on_plane(const dealii::Triangulation<3>::active_face_iterator &face,
+                      const dealii::Point<3> &point_on_plane,
+                      const unsigned int axis);
 
-    static bool is_face_in_circle(dealii::Triangulation<3, 3>::active_face_iterator face,
-                                  const dealii::Point<3> &center,
-                                  const double radius,
-                                  const unsigned int axis);
+bool is_face_in_circle(dealii::Triangulation<3, 3>::active_face_iterator face,
+                       const dealii::Point<3> &center,
+                       const double radius,
+                       const unsigned int axis);
 
-    static bool is_cell_between_two_planes(dealii::TriaActiveIterator<dealii::CellAccessor<3, 3>> cell,
-                                           const dealii::Point<3> point_on_first_plane,
-                                           const dealii::Point<3> point_on_second_plane,
-                                           const unsigned int axis);
-private:
-    static bool is_point_on_sphere(const dealii::Point<3> &point,
-                                   const dealii::Point<3> &center,
-                                   const double radius);
-    static bool is_point_in_sphere(const dealii::Point<3> point,
-                                   const dealii::Point<3> &center,
-                                   const double radius);
-    static bool is_point_on_half_sphere(const dealii::Point<3> point,
-                                        const dealii::Point<3> &center,
-                                        const double radius,
-                                        const dealii::Point<3> &orientation);
-    static bool is_point_between_two_planes(const dealii::Point<3> point,
-                                            const dealii::Point<3> point_on_first_plane,
-                                            const dealii::Point<3> point_on_second_plane,
-                                            unsigned axis);
+bool is_cell_between_two_planes(dealii::TriaActiveIterator<dealii::CellAccessor<3, 3>> cell,
+                                const dealii::Point<3> point_on_first_plane,
+                                const dealii::Point<3> point_on_second_plane,
+                                const unsigned int axis);
+bool is_point_on_sphere(const dealii::Point<3> &point,
+                        const dealii::Point<3> &center,
+                        const double radius);
+bool is_point_in_sphere(const dealii::Point<3> point,
+                        const dealii::Point<3> &center,
+                        const double radius);
+bool is_point_on_half_sphere(const dealii::Point<3> point,
+                             const dealii::Point<3> &center,
+                             const double radius,
+                             const dealii::Point<3> &orientation);
+bool is_point_between_two_planes(const dealii::Point<3> point,
+                                 const dealii::Point<3> point_on_first_plane,
+                                 const dealii::Point<3> point_on_second_plane,
+                                 unsigned axis);
 
-    static dealii::Point<3> get_axis_from_number(const unsigned int axis_no);
-    static bool fuzzy_equal(const double x, const double y);
-};
+dealii::Point<3> get_axis_from_number(const unsigned int axis_no);
+bool fuzzy_equal(const double x, const double y);
+}
