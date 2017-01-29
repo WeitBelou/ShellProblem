6decf33195ffede2062a9093c08cb63567277faa
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-01-29 18:13:25 +0300

    Replace gmsh mesh with one created by hand.

--- src/shell/mesh/IceIslandWithLegsMesh.cpp
+++ src/shell/mesh/IceIslandWithLegsMesh.cpp
@@ -14,14 +14,34 @@ IceIslandWithLegsMesh::IceIslandWithLegsMesh(unsigned int n_global_refinements,
 void IceIslandWithLegsMesh::create_coarse_mesh()
 {
     dealii::Triangulation<3> cube;
-    dealii::GridGenerator::subdivided_hyper_cube(cube, 3, 0, a);
+    const unsigned n = 11;
+    const double c = a / 11.0;
+    dealii::GridGenerator::subdivided_hyper_cube(cube, n, 0, a);
 
     dealii::Triangulation<3> leg;
     dealii::GridGenerator::hyper_rectangle(leg,
-                                           dealii::Point<3>(a / 3, a / 3, a),
-                                           dealii::Point<3>(2 * a / 3, 2 * a / 3, a + h));
-
+                                           dealii::Point<3>(3 * c, 5 * c, a),
+                                           dealii::Point<3>(4 * c, 6 * c, a + h));
     dealii::GridGenerator::merge_triangulations(cube, leg, tria);
+    leg.clear();
+
+    dealii::GridGenerator::hyper_rectangle(leg,
+                                           dealii::Point<3>(7 * c, 5 * c, a),
+                                           dealii::Point<3>(8 * c, 6 * c, a + h));
+    dealii::GridGenerator::merge_triangulations(tria, leg, tria);
+    leg.clear();
+
+    dealii::GridGenerator::hyper_rectangle(leg,
+                                           dealii::Point<3>(5 * c, 7 * c, a),
+                                           dealii::Point<3>(6 * c, 8 * c, a + h));
+    dealii::GridGenerator::merge_triangulations(tria, leg, tria);
+    leg.clear();
+
+    dealii::GridGenerator::hyper_rectangle(leg,
+                                           dealii::Point<3>(5 * c, 3 * c, a),
+                                           dealii::Point<3>(6 * c, 4 * c, a + h));
+    dealii::GridGenerator::merge_triangulations(tria, leg, tria);
+    leg.clear();
 }
 
 void IceIslandWithLegsMesh::apply_manifold_ids()
