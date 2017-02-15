aa2d15e9b19a48bb270c914db53b32ff5aad11a9
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-02-15 22:13:46 +0300

    Update simple island mesh.

--- src/shell/mesh/CylinderMesh.cpp
+++ src/shell/mesh/CylinderMesh.cpp
@@ -23,14 +23,15 @@ void CylinderMesh::create_coarse_mesh()
     const double a = radius / sqrt(2);
     dealii::Point<3> left_bottom(-a, -a, -height / 2);
     dealii::Point<3> right_top(a, a, height / 2);
+    std::vector<unsigned> repetitions{1, 1, 1};
 
-    dealii::GridGenerator::hyper_rectangle(tria, left_bottom, right_top);
+    dealii::GridGenerator::subdivided_hyper_rectangle(tria, repetitions, left_bottom, right_top);
     dealii::GridTools::shift(center, tria);
 }
 
 void CylinderMesh::apply_manifold_ids()
 {
-    static const dealii::CylindricalManifold<3> cylindrical_manifold(2);
+    static const dealii::CylindricalManifold<3> cylindrical_manifold(dealii::Point<3>(0, 0, 1), center);
     static const dealii::FlatManifold<3> flat_manifold;
     tria.set_manifold(0, flat_manifold);
     tria.set_manifold(1, cylindrical_manifold);
@@ -39,7 +40,7 @@ void CylinderMesh::apply_manifold_ids()
     for (auto cell: tria.active_cell_iterators()) {
         for (unsigned f = 0; f < dealii::GeometryInfo<3>::faces_per_cell; ++f) {
             auto face = cell->face(f);
-            if (face->at_boundary() && MeshUtilities::fuzzy_equal(face->center()(2), 0) ) {
+            if (face->at_boundary() && MeshUtilities::fuzzy_equal((face->center() - center)[2], 0)) {
                 face->set_all_manifold_ids(1);
             }
         }
