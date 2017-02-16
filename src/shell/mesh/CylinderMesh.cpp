6fcc7f035beaaf192dd5702f21cdaa2fa40fd113
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-02-16 16:16:02 +0300

    Add material and boundary ids.

--- src/shell/mesh/CylinderMesh.cpp
+++ src/shell/mesh/CylinderMesh.cpp
@@ -20,31 +20,16 @@ CylinderMesh::CylinderMesh(const double radius,
 
 void CylinderMesh::create_coarse_mesh()
 {
-    const double a = radius / sqrt(2);
-    dealii::Point<3> left_bottom(-a, -a, -height / 2);
-    dealii::Point<3> right_top(a, a, height / 2);
-    std::vector<unsigned> repetitions{1, 1, 1};
-
-    dealii::GridGenerator::subdivided_hyper_rectangle(tria, repetitions, left_bottom, right_top);
+    dealii::GridGenerator::cylinder(tria, radius, height / 2);
+    dealii::GridTools::rotate(dealii::numbers::PI_2, 1, tria);
     dealii::GridTools::shift(center, tria);
 }
 
 void CylinderMesh::apply_manifold_ids()
 {
-    static const dealii::CylinderBoundary<3> cylindrical_boundary(radius, dealii::Point<3>(0, 0, 1), center);
-    static const dealii::FlatManifold<3> flat_manifold;
-    tria.set_manifold(0, flat_manifold);
-    tria.set_manifold(1, cylindrical_boundary);
-
+    static const dealii::CylindricalManifold<3> cylindrical_manifold(dealii::Point<3>(0, 0, 1), center);
+    tria.set_manifold(0, cylindrical_manifold);
     tria.set_all_manifold_ids(0);
-    for (auto cell: tria.active_cell_iterators()) {
-        for (unsigned f = 0; f < dealii::GeometryInfo<3>::faces_per_cell; ++f) {
-            auto face = cell->face(f);
-            if (face->at_boundary() && MeshUtilities::fuzzy_equal((face->center() - center)[2], 0)) {
-                face->set_all_manifold_ids(1);
-            }
-        }
-    }
 }
 
 void CylinderMesh::apply_boundary_ids()
@@ -53,7 +38,13 @@ void CylinderMesh::apply_boundary_ids()
         for (unsigned f = 0; f < dealii::GeometryInfo<3>::faces_per_cell; ++f) {
             auto face = cell->face(f);
             if (face->at_boundary()) {
-                face->set_all_boundary_ids(0);
+                auto face_center_z = (face->center() - center)[2];
+                if (MeshUtilities::fuzzy_equal(face_center_z, height / 2)) {
+                    face->set_all_boundary_ids(1);
+                }
+                else if (MeshUtilities::fuzzy_equal(face_center_z, -height / 2)) {
+                    face->set_all_boundary_ids(2);
+                }
             }
         }
     }
@@ -62,7 +53,7 @@ void CylinderMesh::apply_boundary_ids()
 void CylinderMesh::apply_material_ids()
 {
     for (auto cell: tria.active_cell_iterators()) {
-        cell->set_material_id(0);
+        cell->set_material_id(1);
     }
 }
 
